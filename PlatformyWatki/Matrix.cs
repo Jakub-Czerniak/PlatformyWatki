e00c0bb090c9984b83a2d2288fe8fd6d81dcb525
Kuba Czerniak <kubaczarny3@gmail.com>
2022-04-05 20:04:41 +0200

    Add project files.

---
+++ PlatformyWatki/Matrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformyWatki
+{
+    public class Matrix
+    {
+        List<List<int>> m;
+
+        public Matrix(int size)
+        {
+            m = new List<List<int>>();
+            List<int> list = new List<int>(size);
+            for (int i = 0; i < size; i++)
+            {
+
+                for (int j = 0; j < size; j++)
+                    list.Add(i);
+                m.Add(list);
+
+            }
+        }
+
+        public void Odd()
+        {
+            
+        }
+
+        public void Even()
+        {
+            Console.WriteLine("Even");
+        }
+
+        public void Whole()
+        {
+            Console.WriteLine("Whole");
+        }
+    }
+}
---
+++ PlatformyWatki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace PlatformyWatki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Matrix m = new Matrix();
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Thread[] threads = new Thread[2];
+            threads[0] = new Thread(m.Odd);
+            threads[1] = new Thread(m.Even);
+            foreach (Thread thread in threads)
+                thread.Start();
+            foreach (Thread thread in threads)
+                thread.Join();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("With threads: " + elapsedMs + " ms");
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            m.Whole();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("No threads: " + elapsedMs + " ms");
+
+
+        }
+    
+    }
+}
+
